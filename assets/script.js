f5536471a2d9524c4328cbef99648f1e71f26983
Ryan Carey <ryaanski18@yahoo.com>
2021-10-21 21:08:25 -0400

    added our fetch

---
+++ assets/script.js
@@ -0,0 +1,44 @@
+const searchBtn = document.getElementById("btn");
+let time = Date.now();
+const publicKey = "c598176713a7f4af235c955e6ab2cdf5";
+const privateKey = "21fe2681db8292d452bae0ffe9b07fb96a43fc39";
+
+console.log(time);
+
+function getHash() {
+  const hash = time + privateKey + publicKey;
+  fetch("https://api.hashify.net/hash/md5/hex?value=" + hash)
+    .then(function (data) {
+      return data.json();
+    })
+    .then(function (md5) {
+      genHash = md5.Digest;
+
+      heroSearch(genHash);
+    });
+}
+
+function heroSearch(hash) {
+  fetch(
+    "http://gateway.marvel.com/v1/public/characters?name=storm&apikey=" +
+      publicKey +
+      "&hash=" +
+      hash +
+      "&ts=" +
+      time
+  )
+    .then(function (response) {
+      if (!response.ok) {
+        throw response.json();
+      }
+
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      renderPic(data);
+    });
+}
+
+function renderPic(data) {}
+searchBtn.addEventListener("click", getHash);
